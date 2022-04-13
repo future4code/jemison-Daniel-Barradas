85e55a0021376e0b138feda0e767bdb6e594f89b
Daniel Barradas <contato.danielbarradas@gmail.com>
2022-04-12 23:57:50 -0300

    Exercicio vs code

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,46 @@
+//Exercícios de Interpretação de Cõdigo
+
+//Exercício 01
+//Resposta: 10
+//          10 , 5
+
+//Ecercício 02
+//Resposta: 10, 10, 10
+
+//Exercício 03
+//Resposta:
+//         let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+//         let diariaDeTrabalho = prompt("Quanto você recebe por dia?")
+//         alert(`Voce recebe ${horasTrabalhadas/diariaDeTrabalho} por hora`)
+
+//Exercícios de Escrita de Código
+
+//Exercício 01
+//Resposta:
+//let nome = prompt ("Qual seu nome?")
+//let idade = prompt ("Qual sua idade?")
+//console.log (typeof nome, typeof idade);
+//console.log ("Olá", nome, "você tem", idade, "anos")
+
+//Foi impresso o tipo string, porque o comando prompt captura a informação e guarda como string.
+
+//Exercício 02
+//let banho = prompt("Você tomou banho hoje?");
+//let exercicio = prompt("Voce se exercitou hoje?");
+//let descanso = prompt("Você já descansou hoje?");
+
+//console.log("Você tomou banho hoje?", banho);
+//console.log("Você já se exercitou hoje?", exercicio);
+//console.log("Voce já descansou hoje?", descanso);
+
+//Exercício 03
+//let a = 10
+//let b = 25
+//c=a
+//a=b
+//b=c
+//console.log("O novo valor de a é", a);
+//console.log("O novo valor de b é", b);
+
+
+
