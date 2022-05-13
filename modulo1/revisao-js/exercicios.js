c1b691750e1f58c3e4685765cf5f927517ab6095
Daniel Barradas <contato.danielbarradas@gmail.com>
2022-05-13 15:29:45 -0300

    Exercício 03

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -15,22 +15,25 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+ function arrayOrdenada(a,b){
+   return a-b;
+ }
+ return array.sort(arrayOrdenada)
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+
 }
 
 // EXERCÍCIO 07
@@ -40,8 +43,9 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
+
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
@@ -50,40 +54,41 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
-}
+
+  }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
-}
+
+  }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
+
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
-function retornaContasComSaldoAtualizado(contas) {
+function retornaContasComSaldoAtualizado(contas) { 
 
-}
+  }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
-}
+
+ }
