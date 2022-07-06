8e17288c58fa4ac27124c060913afde52adadb13
Daniel Barradas <contato.danielbarradas@gmail.com>
2022-07-05 23:51:39 -0300

    estados-insta4

---
+++ modulo-3/estados-insta4/insta4/src/components/Post/Post.js
@@ -0,0 +1,115 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+
+function Post(props){
+  const [state, setState] = useState({
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
+  })
+
+  const [numeroCurtidas, setnumeroCurtidas] = useState (0)
+  const [curtido, setCurtido] = useState(false)
+  const [comentando, setComentando] = useState(false)
+  const [numeroComentarios, setNumeroComentarios] = useState(0)
+
+  const onClickCurtida = () => {
+    console.log('Curtiu!')
+  }
+  
+  const onClickComentario = () => {
+    setComentando(!comentando)
+    if(comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
+    }
+    console.log(comentando)
+  }
+  
+  const aoEnviarComentario = () => {
+    setComentando(false)
+    setNumeroComentarios(numeroComentarios + 1)
+  }
+
+  let iconeCurtida
+
+    if(curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
+    }
+
+  return(
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          valorContador={state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          valorContador={numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  )
+}
+
+
+export default Post
